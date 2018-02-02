bff7c7527df157bc44ee24e63645d899f96d8ff6
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-02-02 00:00:23 -0400

    First component created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 
 const contentNode = document.getElementById('contents');
+
+class IssueList extends React.Component {
+    render(){
+        return(
+            <div> This is a place holder for the issue list. </div>
+        );
+    }
+}
+
+
 const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
 const message = continents.map( c => `Hello ${c}!`).join( ' - ' );
 var component = <p>{message}</p>;
-ReactDOM.render(component, contentNode);
+ReactDOM.render(<IssueList />, contentNode);
