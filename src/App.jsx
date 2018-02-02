1c429ee404804a348e1a815ddbf54e436346476e
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-02-02 00:17:50 -0400

    Add: Many components into a single file for testing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
 
 const contentNode = document.getElementById('contents');
 
+
+class IssueFilter extends React.Component {
+    render () {
+        return (
+            <div>
+                This is a placeholder for de filter
+            </div>
+        )
+    }
+}
+
+class IssueTable extends React.Component{
+    render(){
+        return(
+            <div>
+                This is a placeholder fr the react table.
+            </div>
+        )
+    }
+}
+
+class IssueAdd extends React.Component{
+    render(){
+        return(
+            <div>
+                This is the form to add the Issue
+            </div>
+        )
+    }
+}
+
 class IssueList extends React.Component {
     render(){
         return(
-            <div> This is a place holder for the issue list. </div>
+            <div>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </div>
         );
     }
 }
